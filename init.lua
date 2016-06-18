3a974c20b1ab15e4e0f4601d0770a7db9acaf907
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2016-06-18 17:28:41 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,18 @@
 minetest.register_craftitem("bandage:bandage", {
 	description = "Bandage",
 	inventory_image = "bandage_bandage.png",
+	
+	minetest.register_on_player_hpchange(function(player, hp_change)
+		if hp_change>2 then
+			user:set_hp(hp + 3)
+			itemstack:take_item()
+		end
+	end
+	
 	on_use = function(itemstack, user, pointed_thing)
 		local hp = user:get_hp()
 		if hp ~= 20 then
-			user:set_hp(hp + 4)
+			user:set_hp(hp + 3)
 			itemstack:take_item()
 		end
 		return itemstack
@@ -17,3 +25,4 @@ minetest.register_craft({
 		{"default:paper", "farming:cotton", "default:paper"}
 	}
 })
+
