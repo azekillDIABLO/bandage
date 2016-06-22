7b779251911e86e8e140d0033f2dd33e9dbb416e
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2016-06-22 14:36:33 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,23 +1,28 @@
 minetest.register_craftitem("bandage:bandage", {
 	description = "Bandage",
 	inventory_image = "bandage_bandage.png",
-	
-	minetest.register_on_player_hpchange(function(player, hp_change)
-		if hp_change>2 then
-			user:set_hp(hp + 3)
-			itemstack:take_item()
-		end
-	end
-	
-	on_use = function(itemstack, user, pointed_thing)
-		local hp = user:get_hp()
-		if hp ~= 20 then
-			user:set_hp(hp + 3)
-			itemstack:take_item()
-		end
-		return itemstack
-	end
 })
+
+minetest.register_craftitem("bandage:bandage", {
+	description = "Bandage",
+	inventory_image = "bandage_bandage.png",
+})
+
+minetest.register_on_player_hpchange(function(player, hp_change)
+	if hp_change >= 0 then
+		return hp_change
+	end
+
+	local inv = player:get_inventory()
+	local itemstack = inv:remove_item("main", ItemStack("bandage:bandage"))
+	if itemstack:get_count() == math.random(1, 3) then
+		-- cancel hp change
+		return -2 
+	else
+		-- no bandages, don't cancel
+		return hp_change
+	end
+end, true)
 
 minetest.register_craft({
 	output = "bandage:bandage",
