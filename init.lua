bf20e63066d96e9659a25a35f123700f09e1a25c
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2016-06-23 15:32:49 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,14 @@
 minetest.register_craftitem("bandage:bandage", {
 	description = "Bandage",
 	inventory_image = "bandage_bandage.png",
+	on_use = function(itemstack, user, pointed_thing)
+		local hp = user:get_hp()
+		if hp ~= 20 then
+			user:set_hp(hp + 4)
+			itemstack:take_item()
+		end
+		return itemstack
+	end
 })
 
 minetest.register_on_player_hpchange(function(player, hp_change)
